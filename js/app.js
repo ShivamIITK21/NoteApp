1789e365217ffd11463cf76c66c992b7a9af623c
Shivam <sshivam21@iitk.ac.in>
2022-05-15 17:45:45 +0530

    added title

--- js/app.js
+++ js/app.js
@@ -4,36 +4,49 @@ showNotes();
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function(e){
     let addTxt = document.getElementById('addTxt');
+    let addTitle = document.getElementById('addTitle');
     let notes = localStorage.getItem('notes');
-    if(addTxt.value != ""){
-        if(notes == null){
+    let titles = localStorage.getItem('titles');
+    if(addTxt.value != "" && addTitle.value != ""){
+        if(notes == null && titles == null){
             notesObj = [];
+            titlesObj = [];
         }
         else{
             notesObj = JSON.parse(notes);
+            titlesObj = JSON.parse(titles);
         }
         notesObj.push(addTxt.value);
+        titlesObj.push(addTitle.value);
         localStorage.setItem("notes",JSON.stringify(notesObj));
+        localStorage.setItem("titles", JSON.stringify(titlesObj));
         addTxt.value = "";
+        addTitle.value = "";
         showNotes();
+    }
+    else{
+        alert("Please fill title and note!");
     }
 })
 
 // showing the note
 function showNotes(){
     let notes = localStorage.getItem('notes');
-    if(notes == null){
+    let titles = localStorage.getItem('titles');
+    if(notes == null && titles == null){
         notesObj = [];
+        titlesObj = [];
     }
     else{
         notesObj = JSON.parse(notes);
+        titlesObj = JSON.parse(titles);
     }
     let html = ``;
     notesObj.forEach(function(element, index){
         html += `
             <div class="card my-2 mx-2 noteCard" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index+1}</h5>
+                    <h5 class="card-title">${titlesObj[index]}</h5>
                     <p class="card-text">${element}</p>
                     <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
@@ -51,14 +64,19 @@ function showNotes(){
 // deleting the note
 function deleteNote(index){
     let notes = localStorage.getItem('notes');
-    if(notes == null){
+    let titles = localStorage.getItem('titles');
+    if(notes == null && titles == null){
         notesObj = [];
+        titlesObj = [];
     }
     else{
         notesObj = JSON.parse(notes);
+        titlesObj = JSON.parse(titles);
     }
     notesObj.splice(index,1);
+    titlesObj.splice(index,1);
     localStorage.setItem("notes", JSON.stringify(notesObj));
+    localStorage.setItem("titles", JSON.stringify(titlesObj));
     showNotes();
 }
 
@@ -66,11 +84,12 @@ function deleteNote(index){
 let search = document.getElementById('searchTxt');
 search.addEventListener('input', function(){
     let inputVal = search.value;
-    console.log('input taken', inputVal);
+    // console.log('input taken', inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function(element){
         let cardTxt = element.getElementsByTagName('p')[0].innerText;
-        if(cardTxt.includes(inputVal)){
+        let cardTitle = element.getElementsByTagName('h5')[0].innerText;
+        if(cardTxt.includes(inputVal) || cardTitle.includes(inputVal)){
             element.style.display = "block";
         }
         else{
